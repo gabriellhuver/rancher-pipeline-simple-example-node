1a465f94a0400f8becfcccebf672ca893cf3e2f0
Gabriell Huver <gabriell.is.huver@gmail.com>
2021-02-27 00:48:44 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -9,8 +9,22 @@ const port = 8080
 var networkInterfaces = os.networkInterfaces();
 
 app.get('/', (req, res) => {
-  res.send(JSON.stringify(os.networkInterfaces))
+  res.send(getIPAddress())
 })
+
+function getIPAddress() {
+    var interfaces = require('os').networkInterfaces();
+    for (var devName in interfaces) {
+      var iface = interfaces[devName];
+  
+      for (var i = 0; i < iface.length; i++) {
+        var alias = iface[i];
+        if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal)
+          return alias.address;
+      }
+    }
+    return '0.0.0.0';
+  }
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
