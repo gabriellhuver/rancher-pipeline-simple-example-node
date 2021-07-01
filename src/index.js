1480d2e26f790d8e01257378dda34b2d9b54a2d7
globalsysgabriell <63593982+globalsysgabriell@users.noreply.github.com>
2021-07-01 10:01:35 -0300

    as

--- src/index.js
+++ src/index.js
@@ -8,25 +8,33 @@ const port = 8080
 
 var networkInterfaces = os.networkInterfaces();
 
-app.get('/api', (req, res) => {
-    console.log('[app-teste] - ' + getIPAddress())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-  res.send(getIPAddress() + " - " + os.hostname() + " - " + JSON.stringify(req.headers))
+app.get('/api', (req, res) => {
+  console.log('[app-teste] - ' + getIPAddress())
+  
+  res.json({
+    ip: getIPAddress(),
+    host: os.hostname(),
+    headers,
+    
+  })
 })
 
 function getIPAddress() {
-    var interfaces = require('os').networkInterfaces();
-    for (var devName in interfaces) {
-      var iface = interfaces[devName];
-  
-      for (var i = 0; i < iface.length; i++) {
-        var alias = iface[i];
-        if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal)
-          return alias.address;
-      }
+  var interfaces = require('os').networkInterfaces();
+  for (var devName in interfaces) {
+    var iface = interfaces[devName];
+
+    for (var i = 0; i < iface.length; i++) {
+      var alias = iface[i];
+      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal)
+        return alias.address;
     }
-    return '0.0.0.0';
   }
+  return '0.0.0.0';
+}
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
